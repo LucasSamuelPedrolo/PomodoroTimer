e86908edcf87e96dba3ea1361c11c24a8d4a9661
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-09-27 23:58:24 -0300

    adicionando ações

--- js/script.js
+++ js/script.js
@@ -3,13 +3,28 @@ const startTimer = document.querySelector('#start');
 const resetTimer = document.querySelector('#reset');
 const pauseTimer = document.querySelector('#pause');
 const btnAboutPomodoro = document.querySelector('#more');
+const animationTimerStart = document.querySelector('.pomodoro-timer-animation');
 
+function disableButton(btn) {
+    btn.disabled = true;
+    btn.style.opacity = '0.2';
+};
+function enableButton(btn) {
+    btn.disabled = false
+    btn.style.opacity = '1';
+}
+
+disableButton(resetTimer);
+disableButton(pauseTimer);
 
 document.addEventListener('click', (e) => {
     const elem = e.target;
 
     if (elem.id === 'start') {
-        console.log(elem.id)
+        disableButton(startTimer);
+        enableButton(pauseTimer);
+        enableButton(resetTimer);
+        animationTimerStart.classList.remove('hidden');
     }
     if (elem.id === 'reset') {
         console.log(elem.id)
@@ -22,3 +37,5 @@ document.addEventListener('click', (e) => {
     }
 
 })
+
+
