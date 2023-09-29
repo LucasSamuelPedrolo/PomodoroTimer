7924014f5d87ad68a2380ddf400da2aca80d1c92
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-09-28 23:58:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const startTimer = document.querySelector('#start');
 const pauseTimer = document.querySelector('#pause');
 const btnAboutPomodoro = document.querySelector('#more');
 
-const minutesToSecondsTimer = 6;
+const minutesToSecondsTimer = 60 * 2;
 let lastMin;
 let timerControl;
 
@@ -24,11 +24,21 @@ document.addEventListener('click', (e) => {
     if (elem.id === 'start') {
         disableButton(startTimer);
         enableButton(pauseTimer);
+
+        if (!lastMin) {
+            clearInterval(timerControl);
+            timerStarted(lastMin, timer);
+            console.log('dentro do last min if')
+        }
+        clearInterval(timerControl);
         timerStarted(minutesToSecondsTimer, timer);
+        console.log('ação aqui');
     }
 
     if (elem.id === 'pause') {
-        console.log(elem.id)
+        clearInterval(timerControl);
+        enableButton(startTimer);
+        disableButton(pauseTimer);
     }
     if (elem.id === 'more') {
 
@@ -37,12 +47,15 @@ document.addEventListener('click', (e) => {
 
 function timerStarted(duration, display) {
     let timer = duration, minutes, seconds;
+
     timerControl = setInterval(() => {
+        console.log('last ', timer + 'e' + lastMin)
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
         minutes = minutes < 10 ? '0' + minutes : minutes;
         seconds = seconds < 10 ? '0' + seconds : seconds;
 
+        lastMin = timer;
         display.textContent = minutes + ':' + seconds;
 
         if (--timer < 0) {
